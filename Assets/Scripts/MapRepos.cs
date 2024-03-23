2f0a36020ed55fa89deb8c9b320ed90fe2417a83
현미 <akshwpsh@gmail.com>
2024-03-24 02:54:39 +0900

    feat : 맵 구현
    **무엇을(What)**
    - 맵 구현
    
    **왜(Why)**
    -
    
    **어떻게(How)**
    -

---
+++ Assets/Scripts/MapRepos.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using FishNet.Object;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MapRepos : MonoBehaviour
+{
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        print("OnTriggerExit2D");
+        if(!other.CompareTag("Area"))
+            return;
+        
+        GameObject player = PlayerController.Instance.gameObject;
+        Vector3 playerPos = player.transform.position;
+        Vector3 myPos = transform.position;
+        float dirX = playerPos.x - myPos.x;
+        float dirY = playerPos.y - myPos.y;
+        
+        float diffx = Mathf.Abs(dirX);
+        float diffy = Mathf.Abs(dirY);
+
+        dirX = dirX > 0 ? 1 : -1;
+        dirY = dirY > 0 ? 1 : -1;
+
+        switch (transform.tag)
+        {
+            case "Ground":
+                if (diffx > diffy)
+                {
+                    transform.Translate(transform.right * dirX * 60);
+                }
+                else if (diffx < diffy)
+                {
+                    transform.Translate(transform.up * dirY * 60);
+                }
+                break;
+        }
+    }
+}
