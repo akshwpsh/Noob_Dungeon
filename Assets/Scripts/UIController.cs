a0602e4d4cfeaf45e7efaa7f0426d06ec5ca117f
GangHyeon <gang01035@naver.com>
2024-03-24 20:17:59 +0900

    refactor : UIController 코드 리팩토링
    **무엇을(What)**
    - UIController코드 리팩토링을
    
    **왜(Why)**
    - 언더패널 UI의 작동방식변화 및 다른 패널과의 일관성을 위해서
    
    **어떻게(How)**
    - 시퀀스를 통한 일괄관리

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -9,14 +9,17 @@ public class UIController : MonoBehaviour
     private bool isMenu;
     public bool isMakeRoom;
     public bool isEnterRoom;
-    public DOTweenAnimation menuUIAnim;
 
     Sequence startMenuSequence;
-    public RectTransform[] PanelList;
+    Sequence MenuBarSequence;
+    public RectTransform[] UIList;
     /// <summary>
     /// 0 -> MainPanel
     /// 1 -> EnderRoomPanel
     /// 2 -> MakeRoomPanel
+    /// 3 -> MenuUI (UnderPanel에 속함)
+    /// 4 -> SettingButton (MenuUI에 속함)
+    /// 5 -> ExitButton(MenuUI에 속함)
     /// </summary>
 
     private void Awake()
@@ -36,47 +39,52 @@ public class UIController : MonoBehaviour
             MenuUIClose();
         }
     }
-    private void MenuUIOpen()
-    {
-        menuUIAnim.DORestartById("Open");
-        isMenu = true;
-    }
-
-    private void MenuUIClose()
-    {
-        menuUIAnim.DORestartById("Close");
-        isMenu = false;
-    }
 
     public void EnterRoomUIOpen()
     {
         startMenuSequence = DOTween.Sequence();
-        startMenuSequence.Append(PanelList[0].DOAnchorPos(new Vector2(500, 0), 0.3f));
-        startMenuSequence.Append(PanelList[1].DOAnchorPos(new Vector2(-30, 30), 0.3f));
+        startMenuSequence.Append(UIList[0].DOAnchorPos(new Vector2(500, 0), 0.3f));
+        startMenuSequence.Append(UIList[1].DOAnchorPos(new Vector2(-30, 30), 0.3f));
         isEnterRoom = true;
     }
 
     public void EnterRoomUIClose()
     {
         startMenuSequence = DOTween.Sequence();
-        startMenuSequence.Append(PanelList[1].DOAnchorPos(new Vector2(500, 30), 0.3f));
-        startMenuSequence.Append(PanelList[0].DOAnchorPos(new Vector2(-30, 0), 0.3f));
+        startMenuSequence.Append(UIList[1].DOAnchorPos(new Vector2(500, 30), 0.3f));
+        startMenuSequence.Append(UIList[0].DOAnchorPos(new Vector2(-30, 0), 0.3f));
         isEnterRoom = false;
     }
 
     public void MakeRoomUIOpen()
     {
         startMenuSequence = DOTween.Sequence();
-        startMenuSequence.Append(PanelList[0].DOAnchorPos(new Vector2(500, 0), 0.3f));
-        startMenuSequence.Append(PanelList[2].DOAnchorPos(new Vector2(0, 200), 0.5f));
+        startMenuSequence.Append(UIList[0].DOAnchorPos(new Vector2(500, 0), 0.3f));
+        startMenuSequence.Append(UIList[2].DOAnchorPos(new Vector2(0, 200), 0.5f));
         isMakeRoom = true;
     }
 
     public void MakeRoomUIClose()
     {
         startMenuSequence = DOTween.Sequence();
-        startMenuSequence.Append(PanelList[2].DOAnchorPos(new Vector2(0, -1000), 0.5f));
-        startMenuSequence.Append(PanelList[0].DOAnchorPos(new Vector2(-30, 0), 0.3f));
+        startMenuSequence.Append(UIList[2].DOAnchorPos(new Vector2(0, -1000), 0.5f));
+        startMenuSequence.Append(UIList[0].DOAnchorPos(new Vector2(-30, 0), 0.3f));
         isMakeRoom = false;
+    }
+    public void MenuUIOpen()
+    {
+        MenuBarSequence = DOTween.Sequence();
+        MenuBarSequence.Append(UIList[3].DOSizeDelta(new Vector2(100, 600), 0.5f));
+        MenuBarSequence.Join(UIList[4].DOAnchorPos(new Vector2(0, 0), 0.2f));
+        MenuBarSequence.Join(UIList[5].DOAnchorPos(new Vector2(0, -130), 0.2f));
+        isMenu = true;
+    }
+    public void MenuUIClose()
+    {
+        MenuBarSequence = DOTween.Sequence();
+        MenuBarSequence.Append(UIList[3].DOSizeDelta(new Vector2(100, 100), 0.4f));
+        MenuBarSequence.Join(UIList[4].DOAnchorPos(new Vector2(0, 0), 0.2f));
+        MenuBarSequence.Join(UIList[5].DOAnchorPos(new Vector2(0, 0), 0.2f));
+        isMenu = false;
     }
 }
